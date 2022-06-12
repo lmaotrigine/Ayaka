f67025fd63997c2d7a5c0075063a43be43c01146
Varun J <root@varunj.me>
2022-06-12 17:43:14 +0530

    cursed commit part 2

--- utils/_types.py
+++ utils/_types.py
@@ -4,12 +4,11 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 
-from typing import TYPE_CHECKING, Literal, TypeAlias, TypedDict
+from typing import Literal, TypeAlias, TypedDict
 
 import discord
 
-if TYPE_CHECKING:
-    from typing_extensions import NotRequired
+from typing_extensions import NotRequired
 
 
 __all__ = (
