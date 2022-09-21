bc04d237011cb1dbe75e28c3288d1c4bc4582bee
Varun <root@5ht2.me>
2022-09-21 18:05:21 +0000

    remove connect four game

---
+++ migrations/V8__remove_connect_four.sql
@@ -0,0 +1,6 @@
+-- Revises: V7
+-- Creation Date: 2022-09-21 18:03:37.901805 UTC
+-- Reason: remove connect four
+
+DROP TABLE IF EXISTS connect_four_games;
+DROP TABLE IF EXISTS connect_four_ranking;
