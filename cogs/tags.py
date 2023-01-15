92b590c94f78d4d202165846da6294476fb731a0
VJ <root@5ht2.me>
2023-01-15 13:46:20 +0530

    Fix tag edit ignoring tag content limits

--- cogs/tags.py
+++ cogs/tags.py
@@ -179,7 +179,7 @@ class Tags(commands.Cog):
         con = connection or self.bot.pool
         query = """SELECT name, content
                     FROM tags
-                    WHERE location_id=$1 
+                    WHERE location_id=$1
                     OFFSET FLOOR(RANDOM() * (
                         SELECT COUNT(*)
                         FROM tags
@@ -681,6 +681,10 @@ class Tags(commands.Cog):
                 await modal.wait()
                 ctx.interaction = modal.interaction
                 content = modal.text
+
+        if len(content) > 2000:
+            await ctx.send('Tag content can only be up to 2000 characters')
+            return
 
         query = "UPDATE tags SET content=$1 WHERE LOWER(name)=$2 AND location_id=$3 AND owner_id=$4;"
         status = await ctx.db.execute(query, content, name, ctx.guild.id, ctx.author.id)
